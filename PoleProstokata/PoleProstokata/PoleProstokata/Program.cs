86a70714f82689b4101febebadecbdbdc29169e8
pdziki84-student <wasza.dzikosc@gmail.com>
2021-11-05 10:52:10 +0100

    Pole prostokata

---
+++ PoleProstokata/PoleProstokata/PoleProstokata/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PoleProstokata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = new double();
+            double b = new double();
+            double pole = new double();
+
+            Console.Write("Podaj wartość pierwszego boku: ");
+            a = double.Parse(Console.ReadLine());
+
+            Console.Write("Podaj wartość drugiego boku: ");
+            b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Pole powierzchni prostkąta o bokach {a} x {b} wynosi: {pole = a * b}");
+
+        }
+    }
+}
