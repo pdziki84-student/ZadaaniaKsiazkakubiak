7d02c7406457fd41a088e73b73c83965d8fb79b0
pdziki84-student <wasza.dzikosc@gmail.com>
2021-11-09 12:27:28 +0100

    reszta z dzielenia

---
+++ ResztaZDzielenia/ResztaZDzielenia/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ResztaZDzielenia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = new int();
+            int b = new int();
+
+
+            a = 37;
+            b = 11;
+
+            Console.WriteLine($"Reszta z dzielenia dwóch liczb {a} i {b} \na % b = " + a%b);
+        }
+    }
+}
