ba560daf515d7406209835c21eae2a40b29ca2c3
pdziki84-student <wasza.dzikosc@gmail.com>
2021-11-09 11:44:26 +0100

    Pierwiastek kwadratowy liczby PI

---
+++ PierwiastekKwadratowyLiczyPI/PierwiastekKwadratowyLiczyPI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PierwiastekKwadratowyLiczyPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pierwiastek kwadratowy PI");
+            Console.WriteLine("{0:#.##}", Math.Sqrt(Math.PI));
+
+            Console.ReadKey();
+        }
+    }
+}
