adfcefd489854038e98394da1d7975a4a496ce1a
pdziki84-student <wasza.dzikosc@gmail.com>
2021-11-09 12:04:35 +0100

    objetosc kuli

---
+++ ObetoscKuli/ObetoscKuli/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ObetoscKuli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double Volume = new double();
+            double Radius = new double();
+
+            Console.Write("Podaj wartość promienia kuli R: ");
+            Radius = double.Parse(Console.ReadLine());
+
+            Volume = (4 * Math.PI * Math.Pow(Radius, 3)) / 3;
+
+            Console.WriteLine("Objętość kuli o promieniu R = {0:##.##} wynosi {1:##.##)}", Radius , Volume);
+        }
+    }
+}
+
+
