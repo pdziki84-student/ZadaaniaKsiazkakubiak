f08cf2dc295fe99d5ed8df152c49e299b4865670
pdziki84-student <wasza.dzikosc@gmail.com>
2021-11-09 12:23:02 +0100

    dzielenie dwoch intow bez reszy

---
+++ DodawanieDwochIntow/DodawanieDwochIntow/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DodawanieDwochIntow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = new int();
+            int b = new int();
+
+            a = 37;
+            b = 11;
+
+
+            Console.WriteLine("Program wyświetla wynik dzielenia całkwitoliczbowego");
+            Console.WriteLine("Dla a = " + a +" i b = " + b );
+
+            Console.WriteLine("a/b = " + a/b );
+        }
+    }
+}
