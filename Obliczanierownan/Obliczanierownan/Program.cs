8c5a472f4b4e738021fced64cac074aacbed6042
pdziki84-student <wasza.dzikosc@gmail.com>
2021-11-10 12:22:18 +0100

    rozzwiazywanie rownan

---
+++ Obliczanierownan/Obliczanierownan/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Obliczanierownan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = new double();
+            double b = new double();
+            double c = new double();
+            double x = new double();
+
+
+            Console.Write("Podaj wartość parametru a: ");
+            a = double.Parse(Console.ReadLine());
+
+            if (a == 0)
+            {
+                Console.WriteLine("Wprowadzono niewłaściwą wartość parametru a!!!");
+            }
+            else
+            {
+                Console.Write("Podaj wartość parametru a: ");
+                b = double.Parse(Console.ReadLine());
+
+                Console.Write("Podaj wartość parametru a: ");
+                c = double.Parse(Console.ReadLine());
+            }
+
+            x = (c - b) / a;
+
+            Console.WriteLine("Dla: \na= {0:##.##}\nb= {1:##.##}\nc= {2:##.##}", a, b, c);
+            Console.WriteLine($"Wynik równania {a}x + {b} = {c} ");
+            Console.WriteLine("wynosi x = {0:##.##}", x);
+
+        }
+    }
+}
+
+
+
