373b747206309b6c188e91b4e481ed47e5a52fe4
pdziki84-student <wasza.dzikosc@gmail.com>
2021-11-14 20:28:59 +0100

    Sumowanie liczb za pomoca petli

---
+++ ForWyswietlanieLiczb/ForWyswietlanieLiczb/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ForWyswietlanieLiczb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For");
+
+            for (int i = 1; i < 21; i++)
+            {
+                Console.Write($"{i}. ");
+            }
+
+            Console.WriteLine("\n\nDo while");
+            int x = 1;
+            do
+            {
+                Console.Write(x +". ");
+                x++;
+            } while (x<21);
+
+            Console.WriteLine("\n\nWhile");
+            int y = 1;
+
+            while (y <= 20)
+            {
+                Console.Write(y +". ");
+                y++;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ SumowanieZeroSto/SumowanieZeroSto/SumowanieZeroSto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SumowanieZeroSto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
