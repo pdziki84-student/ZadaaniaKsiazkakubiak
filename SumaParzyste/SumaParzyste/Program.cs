1e2b668c76a61df0f356fc1f00f440e9a84caa08
pdziki84-student <wasza.dzikosc@gmail.com>
2021-11-14 21:14:38 +0100

    Sumowanie liczb parzystych i nieparzystych

---
+++ SumaParzyste/SumaParzyste/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace SumaParzyste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    suma = suma + i;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            Console.WriteLine("For suma = " + suma);
+
+            int suma1 = 0, j = 0;
+            Console.WriteLine("\n\nDo While");
+
+            do
+            {
+                if (j %2 == 0)
+                {
+                    suma1 = suma1 + j;
+                }
+                j++;
+            } while (j < 101);
+            Console.WriteLine("DoWhile = " + suma1);
+            Console.WriteLine("\n\nWhile");
+
+            int suma2 = 0, k = 0;
+
+            while (k <= 100)
+            {
+                if (k % 2 == 0)
+                {
+                    suma2 = suma2 + k;
+                }
+                k++;
+            }
+            Console.WriteLine("while = " + suma2);
+        }
+    }
+}
---
+++ SumowanieZeroSto/SumowanieZeroSto/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SumowanieZeroSto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For");
+            int suma = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                suma = suma + i;
+            }
+            Console.WriteLine("Suma liczb od 1 do 100 to: " + suma);
+
+            Console.WriteLine("\n\nDo While");
+            
+            int suma1 = 0, id = 1;
+
+            do
+            {
+                suma1 = suma1 + id;
+                id++;
+            } while (id < 101);
+            Console.WriteLine("Suma liczb to : " + suma1);
+
+            Console.WriteLine("\n\nWhile");
+            int suma2 = 0, j = 1;
+
+            while (j <= 100)
+            {
+                suma2 = suma2 + j;
+                j++;
+            }
+            Console.WriteLine("Suma to: " + suma2);
+        }
+    }
+}
