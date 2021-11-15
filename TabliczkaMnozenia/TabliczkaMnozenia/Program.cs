f0edd66bb6e6cd50da1d494aadc49a5cab0dd77f
pdziki84-student <wasza.dzikosc@gmail.com>
2021-11-15 12:55:38 +0100

    tabliczka mnozenia

---
+++ TabliczkaMnozenia/TabliczkaMnozenia/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TabliczkaMnozenia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= n; col++)
+                {
+                    Console.Write(row * col +"\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
